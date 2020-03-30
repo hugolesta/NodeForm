3719134bd72fad6308f26a7aae21e86c702991f7
Hugo Lesta <hlesta@edrans.com>
2020-03-29 21:11:21 -0300

    Added two new functions and logic for installing tfenv.

--- utils/tfenv.js
+++ utils/tfenv.js
@@ -1,14 +1,16 @@
 const GITURL = "https://github.com/tfutils/tfenv.git";
+const TFENVPATH = "~/.tfenv"
 const commandExists = require('command-exists');
-
-const checkIfTfenvWasInstalled = async() => {
-    return new Promise( (resolve, reject ) => {
+const shell = require('shelljs');
+const checkIfTfenvWasInstalled = (program) => {
+    return new Promise( async (resolve, reject ) => {
         try {
-            commandExists('tfenv', (err, commandExists) => {
-                resolve(commandExists);
+            commandExists(program, (err, commandExists) => {
                 if(err){
                     reject(err);
                 }
+                commandExists ? console.log(`${program} is already installed`) : console.log(`${program} is not installed in your S.O`);
+                resolve(commandExists);
             });
         } catch (error) {
             reject(error);
@@ -16,6 +18,49 @@ const checkIfTfenvWasInstalled = async() => {
     });
 };
 
+const installTfEnv = () => {
+    return new Promise( async (resolve, reject) =>{
+        try {
+            //check if tfenv was installed.
+            let tfenvStatus = await checkIfTfenvWasInstalled('tfenv');
+            
+            if(!tfenvStatus) {
+
+                let brewStatus = await checkBrewExists('tfenv');
+
+                if(!brewStatus) {
+                    //Clone tfenv and install manually
+                    let cloneCode = await shell.exec(`git clone ${GITURL} ${TFENVPATH}`).code;
+                    console.log(`Cloning tfenv repo and it was returned the following code ${cloneCode}`)
+                    if(cloneCode === 0) await shell.exec(`ln -s ${TFENVPATH}/bin/* /usr/local/bin`);
+                    console.log()
+                }
+            }
+
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
+const checkBrewExists = (program) => {
+    return new Promise( async (resolve, reject) => {
+        try {
+            let brewStatus = await checkIfTfenvWasInstalled('brew');
+            if(brewStatus) {
+                let status = await shell.exec(`brew install ${program}`);
+                console.log(`bre return the following code: ${status.code}`);
+                resolve(true)
+            } else {
+                resolve(false)
+            }
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+
 module.exports = {
-    checkIfTfenvWasInstalled
+    checkIfTfenvWasInstalled,
+    installTfEnv
 }
