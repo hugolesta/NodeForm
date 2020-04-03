7f1fda227add9e86401e6a064eb344fad09ca353
Hugo Lesta <hlesta@edrans.com>
2020-04-03 16:48:04 -0300

    Added json structure for terrafile

---
+++ utils/terrafile.js
@@ -0,0 +1,10 @@
+module.exports = {
+    terraform_modules : [
+        {
+            name: "terraform-aws-alb",
+            source: "git@github.com:cloudposse/terraform-aws-alb",
+            version: "0.7.0",
+            dependency: ""
+        }
+    ]
+}
