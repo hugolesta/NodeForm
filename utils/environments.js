5555fa36ace31f83e5a85f4c9b61d058a9973770
Hugo Lesta <hlesta@edrans.com>
2020-04-03 16:47:41 -0300

    createEnvironmentFolder function has been completed

--- utils/environments.js
+++ utils/environments.js
@@ -5,8 +5,10 @@ const createEnvironmentFolder = (folderName) => {
     return new Promise( async (resolve, reject) => {
         try {
             if (!fs.existsSync(`./environments/`)) await fs.mkdirSync(`./environments/`);
-            if (!fs.existsSync(`./environments/${folderName}`)) await fs.mkdirSync(`./environments/${folderName}`);
-            console.log(`Folder ./environments/${folderName} has been created`);
+            if (!fs.existsSync(`./environments/${folderName}`)) {
+                await fs.mkdirSync(`./environments/${folderName}`)
+                console.log(`Folder ./environments/${folderName} has been created`);
+            };
             resolve();
         } catch (error) {
             reject(error)
