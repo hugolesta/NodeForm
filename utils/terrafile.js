b1e27435eacff31f5d26bf028b4cef1e003ed2ff
Hugo Lesta <hlesta@edrans.com>
2020-04-03 17:12:26 -0300

    Added container_definition module

--- utils/terrafile.js
+++ utils/terrafile.js
@@ -5,6 +5,12 @@ module.exports = {
             source: "git@github.com:cloudposse/terraform-aws-alb",
             version: "0.7.0",
             dependency: ""
+        },
+        {
+            name: "container_definition",
+            source: "git@github.com:cloudposse/terraform-aws-ecs-container-definition",
+            version: "0.21.0",
+            dependency: ""
         }
     ]
 }
