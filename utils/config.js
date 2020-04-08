17ae201b77cc38f73a70e27eeaf804ecfb23ff4e
Hugo Lesta <hlesta@edrans.com>
2020-04-08 08:27:50 -0300

    Added joi schema here and promptQuestions

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,34 @@
+const Joi = require('@hapi/joi');
+
+const schema = Joi.object().keys({
+    name: Joi.string()
+                .alphanum()
+                .min(3)
+                .max(30)
+                .required(),
+    source: Joi.string()
+                .pattern(/^([A-Za-z0-9]+@|http(|s)\:\/\/)([A-Za-z0-9.]+(:\d+)?)(?::|\/)([\d\/\w.-]+?)(\.git)?$/i)
+                .min(3)
+                .max(60)
+                .required(),
+    version: Joi.string()
+                .alphanum()
+                .min(3)
+                .max(30)
+                .required()
+
+});
+
+
 module.exports = {
     'TFENVGITURL' : "https://github.com/tfutils/tfenv.git",
     'TFENVPATH': "~/.tfenv",
     'TFBINARY' : "terraform",
-    'AWS_REGION': "us-east-1"
+    'AWS_REGION': "us-east-1",
+    'promptQuestions' : [
+        {name: 'name', required: true, description : "Please add new module name."}, 
+        {name: 'source', required: true, description : "Please add new module url."}, 
+        {name: 'version', required: true, description : "Please add new module release version."}, 
+    ],
+    'schema': schema
 }
