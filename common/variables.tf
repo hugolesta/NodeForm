c62632da8f451f3c32dc518e3e163c96e35895dd
Hugo Lesta <hlesta@edrans.com>
2020-04-08 09:52:11 -0300

    Added variable env

---
+++ common/variables.tf
@@ -0,0 +1,13 @@
+variable "env" {
+  description = "Map containing all the environment configuration"
+  type        = map(string)
+  default = {
+    name       = ""
+    prefix     = ""
+    region     = "us-east-1"
+    key_name   = "default"
+    project    = "terraform-automation"
+    costCenter = "Cloud Emgineers"
+    owner      = "default"
+  }
+}
