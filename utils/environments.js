7aa7c365aab20cb1745c95a099e50555c22903b3
Hugo Lesta <hlesta@edrans.com>
2020-04-26 15:54:18 -0300

    removed async

--- utils/environments.js
+++ utils/environments.js
@@ -1,13 +1,24 @@
 const fs = require('fs');
+const fsExtra = require('fs-extra');
 
-const createEnvironmentFolder = (folderName) => {
-    return new Promise( async (resolve, reject) => {
+const createEnvironmentFolder = (folderName,stack) => {
+    return new Promise( (resolve, reject) => {
         try {
-            if (!fs.existsSync(`./environments/`)) await fs.mkdirSync(`./environments/`);
+            let tfvarsFile = `./environments/${folderName}/${stack}/${folderName}.auto.tfvars`;
+            let vpcTfvarsFile = `./environments/${folderName}/vpc/${folderName}.auto.tfvars`;
+            if (!fs.existsSync(`./environments/`)) fs.mkdirSync(`./environments/`);
             if (!fs.existsSync(`./environments/${folderName}`)) {
-                await fs.mkdirSync(`./environments/${folderName}`)
+                fs.mkdirSync(`./environments/${folderName}`);
                 console.log(`Folder ./environments/${folderName} has been created`);
             };
+            if(!fsExtra.ensureFileSync(vpcTfvarsFile)){
+                fs.openSync(vpcTfvarsFile,'w');
+                console.log(`The file ${vpcTfvarsFile} has been created inside stack ${stack}`);
+            }
+            if(!fsExtra.ensureFileSync(tfvarsFile)) {
+                fs.openSync(tfvarsFile,'w');
+                console.log(`The file ${folderName}.auto.tfvars has been created`);
+            }
             resolve();
         } catch (error) {
             reject(error)
